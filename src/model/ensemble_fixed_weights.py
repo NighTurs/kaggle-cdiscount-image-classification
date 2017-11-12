85681a4b3e622ed6cebafebc05dd981fde901a52
NighTurs <nighturs@gmail.com>
2017-11-12 14:24:25 +0300

    Better performance and memory consumption of fixed weights ensemble script

--- src/model/ensemble_fixed_weights.py
+++ src/model/ensemble_fixed_weights.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 import argparse
+import itertools
+from tqdm import tqdm
+from collections import namedtuple
 
 TOP_PREDS = 10
 PREDICTIONS_FILE = 'predictions.csv'
@@ -30,11 +33,44 @@ if __name__ == '__main__':
         all_preds.append(preds)
 
     all_preds = pd.concat(all_preds)
-    print(all_preds.info(memory_usage='deep'))
-    all_preds = all_preds.groupby(['product_id', 'img_idx', 'category_idx'], as_index=False).sum()
-    all_preds.sort_values('prob', inplace=True, ascending=False)
-    all_preds = all_preds.groupby(['product_id', 'img_idx'], as_index=False).head(TOP_PREDS)
+    all_preds.sort_values(['product_id', 'img_idx', 'category_idx'], inplace=True)
+
+    prev_img = (0, 0)
+    prev_cat = 0
+    sum_prob = 0
+    sum_all_probs = 0
+    cat_prob = []
 
     if not os.path.isdir(model_dir):
         os.mkdir(model_dir)
-    all_preds.to_csv(os.path.join(model_dir, PREDICTIONS_FILE), index=False)
+
+    with tqdm(total=all_preds.shape[0]) as pbar, open(os.path.join(model_dir, PREDICTIONS_FILE), 'w') as out:
+        out.write('product_id,img_idx,category_idx,prob\n')
+        for row in itertools.chain(all_preds.itertuples(),
+                                   [namedtuple('Pandas', ['product_id', 'img_idx', 'category_idx', 'prob'])(0, 0, 0,
+                                                                                                            0)]):
+            product_id = row.product_id
+            img_idx = row.img_idx
+            category_idx = row.category_idx
+            prob = row.prob
+            if prev_img == (product_id, img_idx):
+                if prev_cat == category_idx:
+                    sum_prob += prob
+                else:
+                    cat_prob.append((prev_cat, sum_prob))
+                    sum_all_probs += sum_prob
+                    prev_cat = category_idx
+                    sum_prob = prob
+            else:
+                cat_prob.append((prev_cat, sum_prob))
+                sum_all_probs += sum_prob
+                cat_prob = sorted(cat_prob, key=lambda x: x[1], reverse=True)[:TOP_PREDS]
+                if prev_img != (0, 0):
+                    for cat in cat_prob:
+                        out.write('{},{},{},{}\n'.format(prev_img[0], prev_img[1], cat[0], cat[1] / sum_all_probs))
+                prev_img = (product_id, img_idx)
+                prev_cat = category_idx
+                sum_prob = prob
+                sum_all_probs = 0
+                cat_prob = []
+            pbar.update()
